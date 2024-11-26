115fa2df006dcec6e222350b1a2b895e6f33e089
Gul Rasulzada <guldastayr@code.edu.az>
2024-11-26 16:15:13 +0400

    refactor: "footer links updated"

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { FaFacebookF } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
-import { FaYoutube } from 'react-icons/fa'
-import { HashLink as Link } from 'react-router-hash-link'
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Footer = () => {
   return (
@@ -11,11 +11,7 @@ const Footer = () => {
       <div className="footer_container container grid">
         <div className="footer_content grid">
           <div>
-            <Link
-              smooth
-              to={'#'}
-              className="footer_logo"
-            >
+            <Link smooth to={"#"} className="footer_logo">
               Travel
             </Link>
             <p className="footer_description">
@@ -27,29 +23,17 @@ const Footer = () => {
               <h3 className="footer_title">About</h3>
               <ul className="footer_links">
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     About Us
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Features
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     News & Blog
                   </Link>
                 </li>
@@ -60,29 +44,17 @@ const Footer = () => {
               <h3 className="footer_title">Company</h3>
               <ul className="footer_links">
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     FAQs
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     History
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Testimonials
                   </Link>
                 </li>
@@ -93,29 +65,17 @@ const Footer = () => {
               <h3 className="footer_title">Contact</h3>
               <ul className="footer_links">
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Call Center
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Support Center
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Contact Us
                   </Link>
                 </li>
@@ -126,29 +86,17 @@ const Footer = () => {
               <h3 className="footer_title">Support</h3>
               <ul className="footer_links">
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Privacy Policy
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Terms & Services
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    smooth
-                    to={'#'}
-                    className="footer_link"
-                  >
+                  <Link smooth to={"#"} className="footer_link">
                     Payments
                   </Link>
                 </li>
@@ -160,32 +108,40 @@ const Footer = () => {
           <div className="footer_social">
             <Link
               smooth
-              to={'https://www.facebook.com/'}
+              to={"https://www.facebook.com/"}
               target="_blank"
+              aria-label="Open Facebook"
+              rel="noopener noreferrer"
               className="footer_social_link"
             >
               <FaFacebookF className="icon" />
             </Link>
             <Link
               smooth
-              to={'https://www.instagram.com/'}
+              to={"https://www.instagram.com/"}
               target="_blank"
+              aria-label="Open Instagram"
+              rel="noopener noreferrer"
               className="footer_social_link"
             >
               <FaInstagram className="icon" />
             </Link>
             <Link
               smooth
-              to={'https://twitter.com/'}
+              to={"https://twitter.com/"}
               target="_blank"
+              aria-label="Open Twitter"
+              rel="noopener noreferrer"
               className="footer_social_link"
             >
               <FaXTwitter className="icon" />
             </Link>
             <Link
               smooth
-              to={'https://www.youtube.com/'}
+              to={"https://www.youtube.com/"}
               target="_blank"
+              aria-label="Open Youtube"
+              rel="noopener noreferrer"
               className="footer_social_link"
             >
               <FaYoutube className="icon" />
@@ -195,7 +151,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
